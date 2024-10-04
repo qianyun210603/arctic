d5271d1c9cbb872d6e585c08075bcf437d5e364c
booksword <qianyun210603@hotmail.com>
2024-10-04 15:18:14 +0800

    release version limit for pymongo

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup(
                       "mockextras",
                       "pandas",
                       "numpy",
-                      "pymongo",
+                      "pymongo>=3.6.0",
                       "pytz",
                       "tzlocal",
                       "lz4",
