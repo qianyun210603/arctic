860e61d44956f9be9a50c39eb968c7944ed25172
booksword <qianyun210603@hotmail.com>
2026-04-04 17:02:22 +0800

    no more python2 print compatibility

--- benchmarks/lz4_tuning/benchmark_lz4.py
+++ benchmarks/lz4_tuning/benchmark_lz4.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 
 import random
 from datetime import datetime as dt
--- tests/util.py
+++ tests/util.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 try:
     import cStringIO as stringio
 except ImportError:
